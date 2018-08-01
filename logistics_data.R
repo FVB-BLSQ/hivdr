b76eb4d6eed776ea4354f00737fe1c61ad72a11d
Grégoire Lurton <gregoire.lurton@gmail.com>
2018-08-01 17:21:26 +0200

    Vizualisation of stocks

--- logistics_data.R
+++ logistics_data.R
@@ -37,10 +37,12 @@ sigl_art[grep('Stock disponible utilisable' , sigl_art$DE_name), 'type'] <- 'sto
 sigl_art[grep('Quantité consommée' , sigl_art$DE_name), 'type'] <- 'consommation'
 sigl_art[grep('Quantité perdue' , sigl_art$DE_name), 'type'] <- 'perte'
 sigl_art[grep('Jours rupture de stock' , sigl_art$DE_name), 'type'] <- 'rupture'
-sigl_art[grep('AZT+3TC+NVP 60 mg+30mg+50mg Sirop' , sigl_art$DE_name), 'line'] <- 'azt_3tc_nvp_sirop'
-sigl_art[grep("ABC+DDI+LPV" , sigl_art$DE_name), 'line'] <- 'abc_ddi_lpv_c'
-
-sigl_art$DE_name[is.na(sigl_art$line)]
+sigl_art[grep('AZT\\+3TC\\+NVP 60 mg\\+30mg\\+50mg Sirop' , sigl_art$DE_name), 'line'] <- 'azt_3tc_nvp_sirop'
+sigl_art[grep('AZT\\+3TC 300mg\\+150mg\\+Cès' , sigl_art$DE_name), 'line'] <- 'azt_3tc_c'
+sigl_art[grep('AZT\\+3TC\\+NVP 300mg\\+150mg\\+200mg Cès' , sigl_art$DE_name), 'line'] <- 'azt_3tc_nvp_c'
+sigl_art[grep('ABC\\+DDI\\+LPV' , sigl_art$DE_name), 'line'] <- 'abc_ddi_lpv_c'
+table(sigl_art$line)
+sigl_art$DE_name[sigl_art$line == ""]
 
 
 sigl_art$line_indicator <- paste(sigl_art$line, sigl_art$type, sep = '-')
@@ -51,21 +53,72 @@ library(tidyr)
 
 sigl_art %>%  group_by(., period, type) %>% summarize(val=mean(value))
 
-
-sample <- sigl_art[sigl_art$orgUnit %in% sample(sigl_art$orgUnit, 16) , c('stock') ]
-
-sample <- subset(sample, select= c(DE_name, orgUnit, value, period))
-
-spread(sample, key=DE_name , value=value)
-
 library(ggplot2)
-ggplot(sample) +
-  geom_line(aes(x=period, y=C2 12.2 AZT+3TC 300mg+150mg+Cès - Stock disponible utilisable, colour=orgUnit))
 
 
-data_art_2 <- group_by(data_art, orgUnit, type) 
-
-data_art_2 <-  summarize(data_art_2, val=mean(value))
+sigl_art_any <- sigl_art[sigl_art$type == 'stock',] %>% group_by(orgUnit) %>% summarize(sum(value))
 
 
-print(data.frame(data_art_2[400:440,]))
+sigl_art_any <- sigl_art_any[sigl_art_any$`sum(value)` >0 ,]
+
+sigl_art_sample <- sigl_art[sigl_art$orgUnit %in% sigl_art_any$orgUnit, ]
+
+sample <- sigl_art_sample[sigl_art_sample$orgUnit %in% sample(sigl_art_sample$orgUnit, 49) & sigl_art_sample$line !=  'azt_3tc_nvp_sirop',  ]
+
+sample$rupture <- FALSE
+sample$rupture[sample$type == 'rupture' & sample$value > 0] <- TRUE
+
+rupture <- subset(sample, rupture == TRUE , select =  c('orgUnit', 'period','rupture','line'))
+
+sample <- sample[sample$type == 'stock', c('line_indicator', 'orgUnit', 'value', 'period','line')]
+
+sample <- merge(sample, rupture , by = c('orgUnit','period','line'), all=TRUE)
+
+sample$rupture[(sample$rupture == TRUE) & !is.na(sample$rupture)] <- sample$value[(sample$rupture == TRUE) & 
+                                                                                         !is.na(sample$rupture)]
+
+periods <- unique(pnls$period)
+orgUnits <- unique(sample$orgUnit)
+
+exp <- data.frame('period'= rep(periods, length(orgUnits)),
+                  'orgUnit' = sort(rep(orgUnits, length(periods)))
+                  )
+
+exp$line <- 'abc_ddi_lpv_c'
+exp2 <- exp3 <- exp
+exp2$line <- 'azt_3tc_c'
+exp3$line <- 'azt_3tc_nvp_c'
+
+exp <- rbind(exp, exp2, exp3)
+
+to_plot <- merge(sample, exp , by = c('orgUnit','period', 'line'), all=TRUE)
+to_plot$month <- as.Date(zoo::as.yearmon(paste0(substr(to_plot$period, 1, 4), '-', substr(to_plot$period, 5,6))))
+
+ggplot(to_plot) +
+  geom_line(aes(x=month, y=value, colour=line)) + 
+  geom_point(aes(x=month, y=value, colour=line), shape= 'x', size=2) +
+  facet_wrap(~orgUnit, scales = 'free_y') + 
+  geom_point(aes(x = month, y = rupture, colour = line, shape = line), alpha=.5 , size = 4)
+
+
+## Observed CMM
+
+
+### SIGL
+
+sigl_conso <- sigl_art[sigl_art$type == 'consommation',]
+
+sigl_conso %>% group_by(orgUnit, line) %>% summarize(mean(value))
+
+### PNLS
+pnls_art_lab <- merge(pnls_art, art_data_elements)
+
+
+
+### CORDAID
+
+
+
+## Cross plot with stocks
+
+## Risk evaluation
